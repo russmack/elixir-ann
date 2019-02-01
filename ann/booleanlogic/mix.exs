18bae5f26f61de2b007dd76bb026ac5edb7b5eaa
russmack <russmackdev@gmail.com>
2019-02-01 18:30:27 +0000

    Fix missing deps parentheses.

--- ann/booleanlogic/mix.exs
+++ ann/booleanlogic/mix.exs
@@ -2,10 +2,12 @@ defmodule Or.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :booleanlogic,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps]
+    [
+        app: :booleanlogic,
+        version: "0.0.1",
+        elixir: "~> 1.0",
+        deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
